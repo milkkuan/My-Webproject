da60058f07ef3bbd05fff08262e2122ee0b60c14
milkkuan <s19t15909@stu.edu.tw>
2021-03-12 14:55:28 +0800

    change html name and add php

---
+++ folder/phphw.php
@@ -0,0 +1,26 @@
+<?php
+    print("Hello,World!!"."\n");
+    print("This is a first programs!!"."\n");
+
+
+    $_a123 = 123;
+    $_A123 = "ZARDs";
+    $Backet;
+    $now = time();
+    print date('Y-M-d H:i:s');
+    sleep(5);
+    $next = time();
+    print "Travel Time: ".($next-$now)." seconds!"
+
+    // 變數 a 沒宣告就使用,會產生 E_NOTICE 的警告
+    echo '$a: '.$a.'<br />';
+    $name = 100;
+    echo '$name: '.$name.'<br />';
+    $name = 'David';
+    echo '$name: '.$name.'<br />';
+    $a = $a + 100;
+    echo '$a: '.$a.'<br />';
+
+
+
+?>
