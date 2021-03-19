8cb2524b304ba627c108f633d4f0148e2c24c314
milkkuan <s19t15909@stu.edu.tw>
2021-03-19 14:10:54 +0800

    change name

--- PHP HW folder/phpeggbox.php
+++ PHP HW folder/phpeggbox.php
@@ -1,9 +1,20 @@
 <?php
-$eggbox =array();
-$eggbox["土鸡"]=10；
+$eggbox = array();
+$eggbox["土鸡"]=10;
 $eggbox[]=20;
 $eggbox[4]=21;
-$eggbox[]=array_push("")
+
+array_push($eggbox,"Apple");
+$eggbox[2]="Pineapple";
 
 print_r($eggbox);
+array_pop($eggbox);
+print_r($eggbox);
+
+array_pop($eggbox);
+print_r($eggbox);
+
+$eggbox=array(10,20,21);
+print_r($eggbox);
+
 ?>
