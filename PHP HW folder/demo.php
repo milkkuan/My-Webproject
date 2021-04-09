8209db506c68f146cb858e912fd924f1d899fe4e
milkkuan <s19t15909@stu.edu.tw>
2021-04-09 16:44:27 +0800

    change name

--- PHP HW folder/demo.php
+++ PHP HW folder/demo.php
@@ -1,13 +1,13 @@
 <?php
 include "paints.php";
 
-$tshirt = new Clothes("白襯衫",1000);
+$tshirt = new Clothes("鬼滅",1000);
 
 printf("衣服名字：%s\n",$tshirt->getName());
 printf("衣服的價錢：%d\n",$tshirt->getPrize());
 printf("衣服清洗方式：%s \n\n",$tshirt->doWash());
 
-$cowpaint = new Paint("鬼洗",3000,100,30);
+$cowpaint = new Paint("之刃",3000,100,30);
 printf("褲子名稱：%s\n",$cowpaint->getName());
 printf("褲子的價錢：%d 元\n",$cowpaint->getPrize());
 printf("褲子的長度：%d CM\n",$cowpaint->getLength());
